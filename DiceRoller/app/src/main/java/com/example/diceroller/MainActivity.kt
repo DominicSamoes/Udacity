1a9493518a059b5f630e08a2a817494becc3ffd5
DominicSamoes <dominicsamo@outlook.com>
2022-05-07 16:57:48 +0200

    Lesson 1 Complete Dice Roller Project

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 
